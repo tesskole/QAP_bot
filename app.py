0fb74a18762e5e023685903fb7e16722703ea273
tesskole <136754924+tesskole@users.noreply.github.com>
2023-06-18 07:40:11 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import telebot
 from config import token, keys
 from extensions import ExchangeException, Exchange
 
-bot = telebot.TeleBot(token)
+bot = telebot.TeleBot("6078329331:AAEDSeoUjwv9KYoOL2RS89nIpT-lmXBsNtM")
 
 
 @bot.message_handler(commands=['start'])
